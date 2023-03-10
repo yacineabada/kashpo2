5f9b286196ea76cdc7d335868f60fdaa852a5953
yacineabada <53357633+yacineabada@users.noreply.github.com>
2023-03-10 11:20:37 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import "@firebase/auth";
 import "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDM7SXaedhzAkIgd_fkupK4X-RPrkAuzk",
-  authDomain: "kashpo-17348.firebaseapp.com",
-  databaseURL: "https://kashpo-17348.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
   projectId: "kashpo-17348",
   storageBucket: "kashpo-17348.appspot.com",
-  messagingSenderId: "375764843430",
-  appId: "1:375764843430:web:a22ac07863a7d520ddad67",
+  messagingSenderId: "",
+  appId: "",
 };
 
 if (!firebase.apps.length) {
